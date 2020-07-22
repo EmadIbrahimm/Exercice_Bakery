8a9071b2345770d5d01c275c8b40e699b8cf615f
Emad <emad.ibrahim533@gmail.com>
2020-07-22 18:46:40 +0200

    bakery_exe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+import Button from './components/core/Button'
+
+class App extends React.Component {
+// constructor(props) {
+//   super(props);
+//     this.state ={
+//       activeTap: add,
+//       items: [],
+
+//     }
+
+// };
+renderButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div>
+          <button type="button" class="btn btn-outline-primary"> </button>
+            Add
+            <Button/>
+       </div>
+        
+        <div>
+          <button type="button" class="btn btn-outline-primary"> </button>
+          List
+        </div>
+        
+        <div>
+          <button type="button" class="btn btn-outline-primary"> </button>
+          Pay
+        </div>
+
+
   );
 }
 
+  render() {
+      return(
+        <div>
+          {this.renderButton()}
+        </div>
+
+
+        // <div>
+        //   <button type="button" class="btn btn-outline-primary"> </button>
+        //     Add
+        //     <Button/>
+        // </div>
+        
+        // <div>
+        //   <button type="button" class="btn btn-outline-primary"> </button>
+        //   List
+        // </div>
+        
+        // <div>
+        //   <button type="button" class="btn btn-outline-primary"> </button>
+        //   Pay
+        // </div>
+
+
+
+      );
+
+  }
+
+}
 export default App;
